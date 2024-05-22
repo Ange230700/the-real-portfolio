c1af64f8fc6af7b8ed823116bc019c405d4bc4de
Ange230700 <kouakouangeericstephane@gmail.com>
2024-05-22 20:34:56 +0200

    working...

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -12,32 +12,6 @@ function Footer() {
         <h1 className="footer__title">KOUAKOU</h1>
 
         <ul className="footer__list">
-          {/* <li className="footer__item">
-            <a href="#home" className="footer__link">
-              Home
-            </a>
-          </li>
-          <li className="footer__item">
-            <a href="#about" className="footer__link">
-              About
-            </a>
-          </li>
-          <li className="footer__item">
-            <a href="#services" className="footer__link">
-              Services
-            </a>
-          </li>
-          <li className="footer__item">
-            <a href="#portfolio" className="footer__link">
-              Portfolio
-            </a>
-          </li>
-          <li className="footer__item">
-            <a href="#contact" className="footer__link">
-              Contact
-            </a>
-          </li> */}
-
           {navLinks.map((navLink) => (
             <li className="footer__item" key={navLink.id}>
               <HashLink className="footer__link" to={`#${navLink.name}`} smooth>
