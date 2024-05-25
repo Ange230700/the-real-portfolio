f325b62f80f878cd82cc001c7d9328d180557c56
Ange230700 <kouakouangeericstephane@gmail.com>
2024-05-25 12:48:09 +0200

    working...

--- client/src/components/common/ScrollUp.jsx
+++ client/src/components/common/ScrollUp.jsx
@@ -1,3 +1,7 @@
+/*
+  eslint-disable jsx-a11y/anchor-is-valid
+*/
+
 function ScrollUp() {
   window.addEventListener("scroll", () => {
     const scrollUp = document.querySelector(".scrollup");
@@ -12,7 +16,17 @@ function ScrollUp() {
 
   return (
     <a href="#" className="scrollup">
-      <img className="scrollup__icon" src="" alt="scroll up" />
+      {/* <img className="scrollup__icon" src="" alt="scroll up" /> */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        id="Bold"
+        viewBox="0 0 24 24"
+        width="45"
+        height="45"
+        fill="#fff"
+      >
+        <path d="M6.414,15.586H17.586a1,1,0,0,0,.707-1.707L12.707,8.293a1,1,0,0,0-1.414,0L5.707,13.879A1,1,0,0,0,6.414,15.586Z" />
+      </svg>
     </a>
   );
 }
